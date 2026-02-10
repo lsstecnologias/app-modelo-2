d3908079a01174ec74adfe8cc7e2d68f27416a35
leosenadev <leo.sse@hotmail.com>
2026-02-10 20:08:13 -0300

    component main

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Main from './components/main';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main class="main" className='container-fluid'>
-        <section id="menu" class="container menu section mt-4">
-          <div class="row isotope-container" data-aos="fade-up" data-aos-delay="200">
-
-            <div class="col-lg-6 menu-item isotope-item filter-starters">
-              <img src="assets/img/menu/lobster-bisque.jpg" class="menu-img" alt="" />
-              <div class="menu-content">
-                <a href="#">Lobster Bisque</a><span>$5.95</span>
-              </div>
-              <div class="menu-ingredients text-start">
-                Lorem, deren, trataro, filede, nerada
-              </div>
-            </div>
-
-
-            <div class="col-lg-6 menu-item isotope-item filter-specialty">
-              <img src="assets/img/menu/bread-barrel.jpg" class="menu-img" alt="" />
-              <div class="menu-content">
-                <a href="#">Bread Barrel</a><span>$6.95</span>
-              </div>
-              <div class="menu-ingredients text-start">
-                Lorem, deren, trataro, filede, nerada
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+      <Main />
     </div>
   );
 }
---
+++ src/components/main/index.js
@@ -0,0 +1,106 @@
+const Main = ()=>{
+    return(
+          <main class="main" className='container-fluid'>
+        <section id="menu" class=" menu section mt-4">
+          <div class="container isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+            <div class="row isotope-container" data-aos="fade-up" data-aos-delay="200">
+
+              <div class="col-lg-6 menu-item isotope-item filter-starters">
+                <img src="assets/img/menu/lobster-bisque.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Lobster Bisque</a><span>$5.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Lorem, deren, trataro, filede, nerada
+                </div>
+              </div>
+
+
+              <div class="col-lg-6 menu-item isotope-item filter-specialty">
+                <img src="assets/img/menu/bread-barrel.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Bread Barrel</a><span>$6.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Lorem, deren, trataro, filede, nerada
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-starters">
+                <img src="assets/img/menu/cake.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Crab Cake</a><span>$7.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  A delicate crab cake served on a toasted roll with lettuce and tartar sauce
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-salads">
+                <img src="assets/img/menu/caesar.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Caesar Selections</a><span>$8.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Lorem, deren, trataro, filede, nerada
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-specialty">
+                <img src="assets/img/menu/tuscan-grilled.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Tuscan Grilled</a><span>$9.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Grilled chicken with provolone, artichoke hearts, and roasted red pesto
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-starters">
+                <img src="assets/img/menu/mozzarella.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Mozzarella Stick</a><span>$4.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Lorem, deren, trataro, filede, nerada
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-salads">
+                <img src="assets/img/menu/greek-salad.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Greek Salad</a><span>$9.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Fresh spinach, crisp romaine, tomatoes, and Greek olives
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-salads">
+                <img src="assets/img/menu/spinach-salad.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Spinach Salad</a><span>$9.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette
+                </div>
+              </div>
+
+              <div class="col-lg-6 menu-item isotope-item filter-specialty">
+                <img src="assets/img/menu/lobster-roll.jpg" class="menu-img" alt="" />
+                <div class="menu-content">
+                  <a href="#">Lobster Roll</a><span>$12.95</span>
+                </div>
+                <div class="menu-ingredients text-start">
+                  Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </section>
+      </main>
+    )
+}
+export default Main;
